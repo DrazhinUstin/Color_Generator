26e428903f4c21bd1c56ac0dd8da8062923540bf
DrazhinUstin <zmagar91@gmail.com>
2022-01-10 14:18:27 +0300

    Set copy color to clipboard, scss media queries

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import invert from 'invert-color';
 
 const SingleColor = ({ rgb, hex, weight }) => {
+    const [alert, setAlert] = useState(false);
+
+    useEffect(() => {
+        let timerID = setTimeout(() => setAlert(false), 3000);
+        return () => clearTimeout(timerID);
+    }, [alert]);
+
+    const clickHandler = () => {
+        navigator.clipboard.writeText(hex);
+        setAlert(true);
+    };
+
     return (
-        <article style={{ backgroundColor: hex, color: invert(rgb, true) }}>
+        <article style={{ backgroundColor: hex, color: invert(rgb, true) }} onClick={clickHandler}>
             <p>{weight}%</p>
             <p>{hex}</p>
+            <p className={`copy ${alert && 'show'}`}>copied!</p>
         </article>
     );
 };
